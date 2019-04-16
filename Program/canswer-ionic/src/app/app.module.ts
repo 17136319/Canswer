887971928947ec3f4ea1ebaf9138e591e385e3d7
Justin Michel <u14369852@tuks.co.za>
2019-04-16 17:46:39 +0200

    List updates........done article........done symptoms.......done googleMaps.....TODO

--- Program/canswer-ionic/src/app/app.module.ts
+++ Program/canswer-ionic/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { MatIcon } from '@angular/material' ;
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
